6d795a781b2e5fdf96453244be522851da88f924
Lucas Rodrigues Alves <lcs2009@gmail.com>
2021-04-16 12:17:08 -0300

    initial commit

---
+++ scripts/Set problem in parent field of CHG.js
@@ -0,0 +1,25 @@
+/*------------ Begins -------------*/
+
+/* 
+ * Module: Business Rule
+ * Name: Set problem in parent field of CHG
+ * Table: problem
+ * When: before
+ * Update = True
+ * Condition: Change request is not empty
+ * Purpose: By default, when we create a change from a problem, it does not relate the change to that problem in the change record. This script sets the problem number (origin of the change) in the Parent field of the created change.
+ */
+
+(function executeRule(current, previous /*null when async*/ ) {
+
+    var problemNumber = current.getUniqueValue(); // Get sys_id of current problem record
+
+    var grCR = new GlideRecord('change_request');
+    if (grCR.get(current.rfc)) {
+        grCR.setValue('parent', problemNumber);
+        grCR.update();
+    }
+
+})(current, previous);
+
+/*------------ END -------------*/
